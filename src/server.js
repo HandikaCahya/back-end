bda1ec3bb45435acd6413bbae054938229aaa9b7
HandikaCahya <handikacw@gmail.com>
2026-06-03 20:04:01 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -102,5 +102,5 @@ app.use(errorHandler);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server running at http://${port}`);
+  console.log(`Server running on port ${port}`);
 });
